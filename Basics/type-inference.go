d12748586b221a76899b7a0af78f052360a29429
sandwich <sandwich@D1706>
2026-06-28 22:07:41 +0800

    add type inference

---
+++ Basics/type-inference.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 42
+	f := 3.14
+	g := 0.867 + 0.5i
+	fmt.Printf("v is type of %T\n", v)
+	fmt.Printf("f is type of %T\n", f)
+	fmt.Printf("g is type of %T\n", g)
+}
